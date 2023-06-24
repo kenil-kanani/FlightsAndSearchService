db479e052e0282d9a9357581e755136ef07c7a8b
kenil-kanani <kenilkananigecbvn@gmail.com>
2023-06-25 01:24:33 +0530

    setup basic dotenv

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
- 
+ const express = require('express');
+
+ const {PORT} = require('../src/config/serverConfig.js')
+
+ const setUpAndStartServer = async () => {
+    // create the express object
+    const app = express();
+    app.listen(PORT , ()=>{
+        console.log(`Server Started at port : ${PORT}`)
+    })
+ }
+
+ setUpAndStartServer();
